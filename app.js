dda4e880746c686fe1cb726f01d8e3eebace3a29
Adalia da Silva <ada.future.work@gmail.com>
2022-03-13 23:20:45 +0100

    finish iteration 4 & 5 , includes no styles

--- app.js
+++ app.js
@@ -28,36 +28,90 @@ spotifyApi
     console.log("Something went wrong when retrieving an access token", error)
   );
 
-// Our routes go here:
-/* app.get("/", (req, res)=> {
-    res.send('homepage')
-});
-
-app.get('/artist-search', (req, res)=>{
-    res.render('Wasabi')
-}) */
-
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.get("/artist-search", (req, res) => {
-  const artistName = req.query.artist;
-  console.log(artistName);
-  spotifyApi.searchArtists(artistName).then((data) => {
-    console.log(
-      "The received data from the API: ",
-      data.body.artists.items[0].images[0].url
-    );
-    
-  const artistImg = data.body.artists.items[0].images[0].url;
-    res.render("artist-search-results", { artistName, artistImg });
+  let searchArtistByName = req.query.artist;
+  /* console.log(searchArtistByName); */
+
+  spotifyApi.searchArtists(searchArtistByName).then((data) => {
+    /* console.log("The received data from the API: ", data.body.artists.items[0].images[0].url); */
+    /* console.log(data.body.artists); */
+
+    let items = data.body.artists.items;
+    let artistName = items[0].name;
+    let artistImg = items[0].images[0].url;
+    let artistId = items[0].id;
+    res.render("artist-search-results", {
+      artistName,
+      artistImg,
+      artistId,
+    });
   });
-  /* .catch((err) =>
+  /*   .catch((err) =>
         console.log("The error while searching artists occurred: ", err)
       ); */
+});
 
-  /* res.render("Wasabi"); */
+app.get("/albums/:artistId", (req, res, next) => {
+  let artistId = req.params.artistId;
+  /* console.log('Here is the ID', artistId); */
+  let albumArray = [];
+
+  spotifyApi.getArtistAlbums(artistId).then((data) => {
+    /* console.log("The received data from the API: ", data); */
+    let albumItems = data.body.items;
+    /* console.log("Here are the Items", albumItems); */
+    let artistName = data.body.items[0].artists[0].name;
+    /* console.log("Here is the artiName: ", artistName); */
+
+    for (let i = 0; i < albumItems.length; i++) {
+      let elem = albumItems[i];
+
+      let albumObj = {
+        name: "",
+        albumImg: "",
+        albumId: "",
+      };
+
+      albumObj.name = elem.name;
+      albumObj.albumImg = elem.images[0].url;
+      albumObj.albumId = elem.id;
+      albumArray.push(albumObj);
+    }
+    /* console.log(albumArray); */
+    res.render("albums", { artistName, albumArray });
+  });
+  /*     .catch((err) =>
+        console.log("The error while searching artists occurred: ", err)
+      ); */
+});
+
+app.get("/albums/tracks/:albumId", (req, res) => {
+  let albumId = req.params.albumId;
+  let tracksArray = [];
+
+  spotifyApi.getAlbumTracks(albumId).then((data) => {
+    let items = data.body.items;
+    let artistName = data.body.items[0].artists[0].name;
+
+    for (let i = 0; i < items.length; i++) {
+      let elem = items[i];
+
+      let tracksObj = {
+        name: "",
+        trackNumber: "",
+      };
+
+      tracksObj.name = elem.name;
+      tracksObj.trackNumber = elem.track_number;
+      tracksArray.push(tracksObj);
+    }
+    console.log("tracks", { artistName, tracksArray });
+    res.render("tracks", { tracksArray });
+  });
 });
 
 app.listen(3000, () =>
